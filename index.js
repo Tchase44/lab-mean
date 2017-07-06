32b2900eb096c8885db6b9148891fd77fc8d7373
Tim Chase <tchase4544@gmail.com>
2017-07-06 16:10:34 -0400

    angular html bootstrapped, starting express routes

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ let parser = require("body-parser")
 
 let app = express()
 
+let Tag = mongoose.model("Tag")
+let Img = mongoose.model("Img")
+
+app.set("port", process.env.PORT || 4444)
+app.use(parser.json({extended:true}))
+app.use("/assets",express.static("public"))
+
+app.get("/*", function(req, res){
+  res.sendFile(__dirname + "/public/app-root.html");
+});
+app.get("/",function(req,res){
+	Img
+})
+app.get("/",function(req,res){
+	Img
+})
+app.post("/",function(req,res){
+	Img
+})
+app.delete("/",function(req,res){
+	Img
+})
+app.put("/",function(req,res){
+	Img
+})
+
+
+
+app.listen(app.get("port"),function(){
+	console.log("Is your refrigerator running??")
+})
