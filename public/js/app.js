42403b12b0002ad0ffa15ed35ec62bf56011a010
Tarik Kazanci <tarikkzc@gmail.com>
2017-07-06 16:55:09 -0400

    angular app

--- public/js/app.js
+++ public/js/app.js
@@ -44,3 +44,35 @@ function Router($stateProvider, $locationProvider) {
     controllerAs: "vm"
   })
 }
+
+function ImgFactory ($resource) {
+  return $resource('/api/images/:id', {}, {
+    update: {method: "PUT"}
+  })
+}
+
+function IndexControllerFunction($state, Img ) {
+  this.images = Img.query()
+  this.newImage = new Img()
+
+  this.create = function() {
+    this.newImage.$save().then (function(image) {
+      $state.reload()
+    })
+  }
+}
+
+function ShowControllerFunction($state, $stateParams, Img ) {
+  this.image = Img.get({id: $stateParams.id})
+
+  this.update = function() {
+    this.image.$update({id: $stateParams.id}).then(function(image) {
+      $state.go("index")
+    })
+  }
+  this.destroy = function(){
+    this.image.$delete({id: $stateParams.id}).then(function() {
+      $state.go("index")
+    })
+  }
+}
